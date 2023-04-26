e57a259cb452919f62fddd67ced09c01e0ac1130
Mikael Rapp <micke.rapp@gmail.com>
2023-04-26 21:16:43 +0200

    chore(eioutil): typos and edge cases

--- eioutil/writer_selfdestruct_inactivity.go
+++ eioutil/writer_selfdestruct_inactivity.go
@@ -9,7 +9,7 @@ import (
 )
 
 // NewWriterCloserWithSelfDestructAfterIdle will add a timeout that when reached the writecloser has been inactive (no writes)
-// for the duration, it will be automatically closed and any future writes will yeild ErrDestructed
+// for the duration, it will be automatically closed and any future writes will yield ErrDestructed
 func NewWriterCloserWithSelfDestructAfterIdle(ctx context.Context, maxIdle time.Duration, wc WriteCloser) WriteCloser {
 	var once sync.Once
 	var closeErr error
@@ -17,18 +17,29 @@ func NewWriterCloserWithSelfDestructAfterIdle(ctx context.Context, maxIdle time.
 		closeErr = wc.Close()
 	}
 
+	lock := make(chan struct{}, 1)
+
 	KA := keepalive.New(ctx, maxIdle, true, func() {
+		lock <- struct{}{}
 		once.Do(onceBody)
+		<-lock // cleanup
+		close(lock)
 	})
 
 	// Let's update the last access both before and after so we don't end up
 	// in a situation where a file that has been written for a long time
 	// is closed directly after the write.
-	writer := NewPrePostWriteCallback(wc, func(_ []byte) error {
+	writer := NewPreWriteCallback(wc, func(_ []byte) error {
 		KA.Ping()
 		if KA.Done() {
 			return ErrAlreadyClosed
 		}
+		lock <- struct{}{}
+		return nil
+	})
+	writer = NewPostWriteCallback(writer, func(_ []byte) error {
+		<-lock
+		KA.Ping()
 		return nil
 	})
 
