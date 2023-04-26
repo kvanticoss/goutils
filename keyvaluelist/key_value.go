05b40e0e1fc83e7b9ecf3c604e1384666aa2b209
Mikael Rapp <micke.rapp@gmail.com>
2023-04-26 21:22:15 +0200

    chore(keyvaluelist): typos

--- keyvaluelist/key_value.go
+++ keyvaluelist/key_value.go
@@ -6,7 +6,7 @@ import (
 	"strings"
 )
 
-// ErrInvalidFormat is returend when FromHadoopPartition fails to parse the input
+// ErrInvalidFormat is returned when FromHadoopPartition fails to parse the input
 var ErrInvalidFormat = fmt.Errorf("invalid format for hadoop partitions; must be QueryEscape({key})=QueryEscape({val})")
 
 // KeyValue is a string tuple used to represent an order list of KeyValue pairs (unlike a map which is un-ordered)
@@ -14,12 +14,12 @@ type KeyValue struct {
 	Key, Value string
 }
 
-// ToHadoopPartition encodes the KeyValue pair into a hadoop style partion string with the key and value QueryEncoded
+// ToHadoopPartition encodes the KeyValue pair into a hadoop style partition string with the key and value QueryEncoded
 func (kv KeyValue) ToHadoopPartition() string {
 	return url.QueryEscape(kv.Key) + "=" + url.QueryEscape(kv.Value)
 }
 
-// FromHadoopPartition encodes the KeyValue pair into a hadoop style partion string with the key and value QueryEncoded
+// FromHadoopPartition encodes the KeyValue pair into a hadoop style partition string with the key and value QueryEncoded
 func FromHadoopPartition(in string) (KeyValue, error) {
 	elems := strings.Split(in, "=")
 	if len(elems) != 2 {
