09c3fe5db7175c19437ace56ea9e6918c45587ec
Mikael Rapp <micke.rapp@gmail.com>
2023-06-28 21:49:58 +0200

    add writerfactory.WithSuffix()

--- writerfactory/with_prefix.go
+++ writerfactory/with_prefix.go
@@ -5,14 +5,26 @@ import (
 	"path"
 )
 
-// WrapWFWithPrefix adds a prefix to all writes conducted by the writerfactory
+// WrapWFWithPrefix adds a path prefix to all writes conducted by the writerfactory
 func WrapWFWithPrefix(wf WriterFactory, prefix string) WriterFactory {
 	return func(writePath string) (io.WriteCloser, error) {
 		return wf(path.Join(prefix, writePath))
 	}
 }
 
-// WithPrefix adds a prefix to all writes conducted by the writerfactory
+// WithPrefix adds a path prefix to all writes conducted by the writerfactory
 func (wf WriterFactory) WithPrefix(prefix string) WriterFactory {
 	return WrapWFWithPrefix(wf, prefix)
+}
+
+// WrapWFWithSuffix adds a path suffix to all writes conducted by the writerfactory
+func WrapWFWithSuffix(wf WriterFactory, suffix string) WriterFactory {
+	return func(writePath string) (io.WriteCloser, error) {
+		return wf(path.Join(writePath, suffix))
+	}
+}
+
+// WithSuffix adds a Suffix to all writes conducted by the writerfactory
+func (wf WriterFactory) WithSuffix(suffix string) WriterFactory {
+	return WrapWFWithSuffix(wf, suffix)
 }
